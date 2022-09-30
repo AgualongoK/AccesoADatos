b0a566936570f1d339703d1d2c3e69faac0b89ca
Kay <92918293+AgualongoK@users.noreply.github.com>
2022-09-30 12:51:52 +0200

    Update Prueba1.py

--- Introduccion/Prueba1.py
+++ Introduccion/Prueba1.py
@@ -1,1 +1,30 @@
 print ("Hola Mundo, estoy programando en Python!")
+
+print("""
+hdeh
+jdjfe
+dnfdj
+ajd
+sjd
+sw
+"""
+)
+
+#Listas-> arrays dinámicos
+objetos = ["casa","coche","libro"]
+print(objetos)
+print(objetos[1])
+
+objetos.append("reloj")
+print(objetos)
+
+objetos.extend("curso de informatica")
+print(objetos)
+
+objetos.insert(3,"cohete")
+print(objetos)
+
+#Listas-> arrays estáticos
+nombres = ("Ricardo","Maria","Laura")
+print(nombres)
+print(nombres[2])
