c80feddd20429b9c1d00cffb15537afaba101331
Kay <92918293+AgualongoK@users.noreply.github.com>
2022-09-30 13:34:10 +0200

    update

--- Introduccion/Prueba1.py
+++ Introduccion/Prueba1.py
@@ -29,9 +29,39 @@ nombres = ("Ricardo","Maria","Laura")
 print(nombres)
 print(nombres[2])
 
-MiDiccionario - {
-    'Nombre' : 'Luismi',
-    'Edad' : 20,
-    'Pais' : 'Espania'
-}
+num1= 2
+num2 = 3
 
+if num1 == num2 : 
+    print("son iguales")
+elif num1 != num2 :
+    print("no son iguales")
+else :
+    print("Error")
+
+"""
+fnuhfdhfhdfhshfd
+dfdjfdhf
+iweoqiueqwe
+poejje
+
+"""
+
+print("texto despues de un comentario de bloque")
+
+anio =  int(input("dime un anio"))
+print(anio)
+
+#Ejercicio de pedir dos anios y compararlos.
+#Si la diferecencia es mas que 100 anios: esto es mayor que un siglo
+#Si la diferencia es menor a 100: esto es menor que un siglo
+
+anio1 = int(input("dime un anio"))
+anio2 = int(input("dime un anio"))
+
+if (anio1 - anio2) > 100 : 
+    print("La diferencia es mayor que un siglo")
+elif (anio1 - anio2) < 100 :
+    print("La diferencia es menor que un siglo")
+else : 
+    print("Errod de comparacion")
