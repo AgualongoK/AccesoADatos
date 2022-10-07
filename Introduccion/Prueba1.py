564adae39104bbd52ab9caa2b2aa787707705e50
Kay <92918293+AgualongoK@users.noreply.github.com>
2022-10-07 10:24:26 +0200

    update

--- Introduccion/Prueba1.py
+++ Introduccion/Prueba1.py
@@ -1,12 +1,15 @@
+"""
 print ("Hola Mundo, estoy programando en Python!")
 
 print("""
+"""
 hdeh
 jdjfe
 dnfdj
 ajd
 sjd
 sw
+"""
 """
 )
 
@@ -39,13 +42,11 @@ elif num1 != num2 :
 else :
     print("Error")
 
-"""
 fnuhfdhfhdfhshfd
 dfdjfdhf
 iweoqiueqwe
 poejje
 
-"""
 
 print("texto despues de un comentario de bloque")
 anio =  int(input("dime un anio"))
@@ -55,7 +56,7 @@ print(anio)
 #Si la diferecencia es mas que 100 anios: esto es mayor que un siglo
 #Si la diferencia es menor a 100: esto es menor que un siglo
 
-"""
+
 anio1 = int(input("dime un anio"))
 anio2 = int(input("dime un anio"))
 
@@ -65,7 +66,7 @@ elif (anio1 - anio2) < 100 :
     print("La diferencia es menor que un siglo")
 else : 
     print("Error de comparacion")
-"""
+
 
 #Ejemplo de un for
 numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
@@ -73,7 +74,7 @@ numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
 #Ejercicio que simule un menu y el usuario introduzca su opcion
 
-"""
+
 opcion = int(input("Selecciona lo que quieres imprimir: 1Pares 2Impares 3Ambos"))
 
 if opcion == 1 :
@@ -102,7 +103,7 @@ palabras = ['Peine','Pelo','Ventana','Refrigerador','Adolescente','Dentista','As
 for caracteres in palabras:
     print((caracteres),(len (caracteres)))
 
-"""
+
 
 # Declarar una lista con una frase, recorrer y decir cuantas 'a' hay
 frase = ["Hola, estoy programando en python"]
@@ -114,3 +115,13 @@ for caracteres in frase:
         contador += contador
 
 print("Hay un total de " + str(contador) + " 'a'")
+
+"""
+
+# Ejercicio: un programa que lea 4 numeros y muestre por pantalla el mayor
+aux = 0
+for i in range(4):
+    m=int(input("Dame un numero: "))
+    if m>aux:
+        aux=m
+print("El numero mayor es:", aux)
