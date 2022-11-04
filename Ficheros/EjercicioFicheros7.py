9775ed5e34f40fcbde3b1435d433d8ee9179cfb7
Kay <katherine.agualongo@cesjuanpablosegundo.es>
2022-11-04 13:29:32 +0100

    update

---
+++ Ficheros/EjercicioFicheros7.py
@@ -0,0 +1,24 @@
+'''
+Ejercicio:
+Escribe un programa que pida por teclado un numero positivo entre 1 y 
+10 y guarde en un fichero llamado "ejercicio3-tabla.txt" las tablas de 
+multiplicar de todos los numeros introducidos desde el 1 hasta el numero
+elegido. Cada tabla debera estar identificada mediante un texto en pantalla:
+Tabla del 1:
+1x0=0
+1x1=1
+.
+.
+Tabla del 2:
+etc
+'''
+numero = int(input('Introduce un numero entero entre 1 y 10'))
+
+with open("ejercicio3-tabla.txt", 'w') as f:
+
+    for indice in range(1, (numero+1)):
+        f.write("\n" + "Tabla del " + str(indice) + "\n")
+        for e in range(1,11):
+                f.write(str(indice) + "x" + str(e) + "=" + str(indice*e) + "\n")
+
+f.close()
