f902c54cc89935a114c8290e9742d36245fb0d05
Kay <katherine.agualongo@cesjuanpablosegundo.es>
2022-11-04 11:19:24 +0100

    update

--- Introduccion/EjercicioFicheros3.py
+++ Introduccion/EjercicioFicheros3.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #Ejercicio: escribir x veces un texto en el fichero
-with open("ficheroenpython.txt", "r+") as f: 
-    contenido = f.read()
+with open("ficheroenpython.txt", "a") as f: 
     for i in range(8):
         f.write("\n Opcion ")
 f.close()
