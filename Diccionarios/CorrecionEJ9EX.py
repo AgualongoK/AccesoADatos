7f1901e64369b3ecf64a90c66d3b48648474bd2b
Kay <katherine.agualongo@cesjuanpablosegundo.es>
2022-12-09 10:42:52 +0100

    update

--- Diccionarios/CorrecionEJ9EX.py
+++ Diccionarios/CorrecionEJ9EX.py
@@ -12,9 +12,19 @@ def Consulta_Errores(fichero2):
         f = open(fichero2,'r')
         directorio = f.readlines()
         print('El fichero con datos de desvio contiene', len(directorio), ' posibles errores de trayectoria')
-        
+
     except FileNotFoundError:
         print('\n El fichero ' + fichero2 + ' no existe! \n')
+
+def menu():
+    print('\n','\n','Gestion del sistema de control de vuelo')
+    print('=========================')
+    print('1- Consultar coordenadas erroneas')
+    print('2- Buscar el error')
+    print('3- Eliminar un telefono')
+    print(' - Terminar')
+    opcionmenu = (input('Introduzca el numero de la opcion deseada: '))
+    return opcionmenu
 
 def inicio():
     file = 'ficheroexam3.txt'
